984b7d01c4b27f748a9d929d468619a095eee250
Zebra <jromanosp@hotmail.com>
2020-01-15 01:56:49 +0100

    im not dumb

--- bin/release.sh
+++ bin/release.sh
@@ -9,7 +9,7 @@ TMP_DIR=tmp
 
 mkdir -p $TMP_DIR
 if ! [ -e $TMP_DIR/$FILE_NAME ]; then
-    curl $HUGO_PACKAGE -L -s -o $CACHE_DIR/$FILE_NAME
+    curl $HUGO_PACKAGE -L -s -o $TMP_DIR/$FILE_NAME
 fi
 
 tar -zxvf $TMP_DIR/$FILE_NAME -C $TMP_DIR
