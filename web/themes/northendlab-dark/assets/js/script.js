30993f632c2f3754d337f6469b3dd60bd66c643a
xzebra <jromanosp@hotmail.com>
2020-06-27 23:25:26 +0200

    navbar update

--- web/themes/northendlab-dark/assets/js/script.js
+++ web/themes/northendlab-dark/assets/js/script.js
@@ -1,13 +1,14 @@
 (function ($) {
   'use strict';
 
-  // Preloader js    
+  let search_open = false;
+
+  // Preloader js
   $(window).on('load', function () {
     $('.preloader').fadeOut(100);
   });
 
   // navigation
-  /*
   $(window).scroll(function () {
     if ($('.navigation').offset().top > 1) {
       $('.navigation').addClass('nav-bg');
@@ -15,14 +16,20 @@
       $('.navigation').removeClass('nav-bg');
     }
   });
-  */
 
-  //  Search Form Open
+  // Search Form Open
   $('#searchOpen').on('click', function () {
     $('.search-wrapper').addClass('open');
+	setTimeout(function() {
+	  $('.search-box').focus();
+	}, 400)
+
+	search_open = true;
   });
   $('#searchClose').on('click', function () {
     $('.search-wrapper').removeClass('open');
+
+	search_open = false;
   });
 
 })(jQuery);
